06041eeeefb91698ca0d5930caaf05c861e50c87
Your Name <your.email@example.com>
2024-09-05 17:14:18 +0800

    feat : input vmodel功能

--- components/Atom/Input.vue
+++ components/Atom/Input.vue
@@ -1,4 +1,5 @@
 <script setup>
+const emit = defineEmits(['update:modelValue'])
 const props = defineProps({
   title: {
     type: String,
@@ -17,12 +18,15 @@ const { title, placeholder, type } = toRefs(props)
 </script>
 
 <template>
-  <div class="flex items-center justify-between px-5">
-    <p class="w-1/5 text-black">{{ title }}</p>
+  <div class="flex items-center justify-center gap-2 px-5">
+    <p v-if="title.length > 0" class="text-black">{{ title }}</p>
     <input
+      @input="emit('update:modelValue', $event.target.value)"
+      :value="modelValue"
       :placeholder="placeholder"
       :type="type"
-      class="w-4/5 rounded-md border-[1px] border-[#a9a9a9] p-2 text-black focus-visible:outline-none"
+      class="rounded-md border-[1px] border-[#a9a9a9] p-2 text-black focus-visible:outline-none"
+      :class="[title.length > 0 ? 'w-4/5' : 'w-full']"
     />
   </div>
 </template>
